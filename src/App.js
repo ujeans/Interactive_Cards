4d07761fede848afc0501c0959617ad6c7ee0644
ujeans <dbwlsxkal95@gmail.com>
2024-04-22 22:27:21 +0900

    router 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import ProjectPage from "./pages/ProjectPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/project" element={<ProjectPage />} />
+    </Routes>
+  );
 }
 
 export default App;
