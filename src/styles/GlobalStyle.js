c7f429b0a06d5eaa75036f7bc7fd50337579b0bb
ujeans <dbwlsxkal95@gmail.com>
2024-06-17 20:51:37 +0900

    폰트 적용

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,7 +1,8 @@
-// src/GlobalStyle.js
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu+Sans:ital,wght@0,100..800;1,100..800&display=swap');
+
   :root {
     --cardW: ${props => props.theme.cardW};
     --cardH: ${props => props.theme.cardH};
@@ -9,19 +10,30 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font: 1em "Open Sans", sans-serif;
     height: 100vh;
     overflow-x: hidden;
+  font-family: "Ubuntu Sans", sans-serif;
+  font-optical-sizing: auto;
+  font-weight: 500;
+  font-style: normal;
+  font-variation-settings:
+    "wdth" 100;
   }
 
   *,
-*:before,
-*:after {
-  border: 0;
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
+  *:before,
+  *:after {
+    border: 0;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  .ubuntu-light {
+    font-family: "Ubuntu", sans-serif;
+    font-weight: 300;
+    font-style: normal;
+  }
 `;
 
 export default GlobalStyle;
