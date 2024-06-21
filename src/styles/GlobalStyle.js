9fb4b20bbee0dd3a2d16c12c7cf5b181c9e39531
ujeans <dbwlsxkal95@gmail.com>
2024-06-21 18:30:43 +0900

    테스트 13

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+ @import url('https://fonts.googleapis.com/css2?family=Ubuntu+Sans:ital,wght@0,100..800;1,100..800&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&display=swap');
+
  *, *:before, *:after {
     border: 0;
     box-sizing: border-box;
@@ -8,7 +11,7 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
   :root {
-    font-size: 18px;
+    /* font-size: 18px; */
     --cardW: ${props => props.theme.cardW};
     --cardH: ${props => props.theme.cardH};
     --cardZInc: ${props => props.theme.cardZInc};
@@ -17,7 +20,21 @@ const GlobalStyle = createGlobalStyle`
     font: 1em "Open Sans", sans-serif;
     height: 100vh;
     overflow-x: hidden;
+
+    font-family: "Ubuntu Sans", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 500;
+    font-style: normal;
+    font-variation-settings:
+      "wdth" 100;
   }
+
+  .ubuntu-light {
+    font-family: "Ubuntu", sans-serif;
+    font-weight: 300;
+    font-style: normal;
+  }
+  
   @media screen and (prefers-color-scheme: dark) {
     body {
       background: #3d3d3d;
